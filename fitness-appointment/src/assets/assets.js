d8b44ca9c0041cb0292b75e04be0f6c3bdc3aa30
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-16 20:18:29 +0530

    create assets.js file for images import and use in app

---
+++ fitness-appointment/src/assets/assets.js
@@ -0,0 +1,62 @@
+import Trainer1 from './Trainer1.jpg'
+import Trainer2 from './Trainer2.jpg'
+import Trainer3 from './Trainer3.jpg'
+import Trainer4 from './Trainer4.jpg'
+import Trainer5 from './Trainer5.jpg'
+import Trainer6 from './Trainer6.jpg'
+import Trainer7 from './Trainer7.jpg'
+import Trainer8 from './Trainer8.jpg'
+import Trainer9 from './Trainer9.jpg'
+import Trainer10 from './Trainer10.jpg'
+import Trainer11 from './Trainer11.jpg'
+import Trainer12 from './Trainer12.jpg'
+import Trainer13 from './Trainer13.jpg'
+import Trainer14 from './Trainer14.jpg'
+import Trainer15 from './Trainer15.jpg'
+
+import about from './about.jpg';
+import appointment from './appointment.jpg';
+import boxing from './boxing.jpg';
+import cardio from './cardio.jpg';
+import contact from './contact.png';
+import cycling from './cycling.jpg';
+import dance from './dance.jpg';
+import heroimgone from  './heroimgone.png';
+import strength from './strength.png';
+import logo from './logo.png';
+import group_profiles from './group_profiles.png'
+import profile_pic from './profile_pic.jpg';
+import dropdown_icon from './dropdown_icon.svg'
+import menu_icon from './menu_icon.svg'
+import cross_icon from './cross_icon.png'
+import chats_icon from './chats_icon.svg'
+import verified_icon from './verified_icon.svg'
+import arrow_icon from './arrow_icon.svg'
+import info_icon from './info_icon.svg'
+import upload_icon from './upload_icon.png'
+import yoga from './yoga.jpg';
+
+
+export const assets = {
+    appointment,
+    heroimgone,
+    group_profiles,
+    logo,
+    chats_icon,
+    verified_icon,
+    info_icon,
+    profile_pic,
+    arrow_icon,
+    contact,
+    about,
+    menu_icon,
+    cross_icon,
+    dropdown_icon,
+    upload_icon,
+    boxing,
+    strength,
+    dance,
+    cycling,
+    cardio,
+    yoga
+}
