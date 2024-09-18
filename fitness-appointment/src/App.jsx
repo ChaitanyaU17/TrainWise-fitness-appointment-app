15eaef1646ba2ad5c7dfaa8ebe0dbac021842efc
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-18 10:22:16 +0530

    add component routes to app component configure paths

--- fitness-appointment/src/App.jsx
+++ fitness-appointment/src/App.jsx
@@ -1,11 +1,31 @@
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Trainers from "./pages/Trainers";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trainers" element={<Trainers />} />
+        <Route path="/trainers/:speciality" element={<Trainers />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:trainerId" element={<Appointment />} />
+      </Routes>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default App;
