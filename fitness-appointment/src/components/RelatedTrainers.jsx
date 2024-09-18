5715da68bb59a99100c2c3d6694321503e51d5e6
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-18 10:09:10 +0530

    categories related to to trainers speciality layout

--- fitness-appointment/src/components/RelatedTrainers.jsx
+++ fitness-appointment/src/components/RelatedTrainers.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 
 const RelatedTrainers = () => {
   return (
-    <div>
-      related trainers
+    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+      <h1 className="text-3xl font-medium">Top Fitness Trainers to Book</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Simply browse through our extensive list of trusted trainers.
+      </p>
+      <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+       
+          <div
+            
+            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+            
+          >
+            <LazyLoad>
+            <img className="bg-blue-50" src={} alt={} />
+            </LazyLoad>
+            <div className="p-4">
+              <div className="flex items-center gap-2 text-sm text-center text-green-500">
+                <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+                <p>Available</p>
+              </div>
+              <p className="text-gray-900 text-lg font-medium">{}</p>
+              <p className="text-gray-600 text-sm">{}</p>
+            </div>
+          </div>
+        
+      </div>
+      <button
+        
+        className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10"
+      >
+        More
+      </button>
     </div>
   )
 }
