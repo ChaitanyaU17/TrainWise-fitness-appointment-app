31946fa464439de64594b9160198d9eaa819d9ea
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-18 09:43:07 +0530

    add booking slotsby date time slots

--- fitness-appointment/src/pages/Appointment.jsx
+++ fitness-appointment/src/pages/Appointment.jsx
@@ -48,6 +48,41 @@ const Appointment = () => {
             </p>
           </div>
         </div>
+        {/* ---- Booking Slots --- */}
+        <div className="sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700">
+          <p>Booking Slots</p>
+          <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4 ">
+            {trainerSlots.length &&
+              trainerSlots.map((item, index) => (
+                <div
+                  onClick={() => setSlotIndex(index)}
+                  className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${
+                    slotIndex === index
+                      ? "bg-primary text-white"
+                      : "border border-gray-50"
+                  } `}
+                  key={index}
+                >
+                  <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                  <p>{item[0] && item[0].datetime.getDate()}</p>
+                </div>
+              ))}
+          </div>
+          <div className="flex items-center w-full gap-3 mt-4 overflow-x-scroll">
+            {trainerSlots.length &&
+              trainerSlots[slotIndex].map((item, index) => (
+                <p
+                  onClick={() => setSlotTime(item.time)}
+                  className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`}
+                  key={index}
+                >
+                  {item.time.toLowerCase()}
+                </p>
+              ))}
+          </div>
+          <button onClick={() => handleBooking()} className="bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6">Book an Appointment</button>
+          <ToastContainer />
+        </div>
         </div>
     </div>
   )
