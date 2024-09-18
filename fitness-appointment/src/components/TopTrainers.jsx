d80e335622ce234e0765c03ddd3c4fd9aed3a1f6
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-18 10:03:24 +0530

    add logic navigation map trainers array in form of cards

--- fitness-appointment/src/components/TopTrainers.jsx
+++ fitness-appointment/src/components/TopTrainers.jsx
@@ -1,49 +1,62 @@
-import React from 'react'
+// import React from 'react'
+
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import LazyLoad from "react-lazyload";
 
 const TopTrainers = () => {
+  const navigate = useNavigate();
+  const { trainers } = useContext(AppContext);
+
   return (
-    <>
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
-    <h1 className="text-3xl font-medium">Top Fitness Trainers to Book</h1>
-    <p className="sm:w-1/3 text-center text-sm">
-      Explore our carefully curated list of trusted fitness trainers.
-    </p>
-    
-    
-    <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0 ">
-     
-        
-          <div className="h-[201px] w-full overflow-hidden">
-            <LazyLoad>
-              <img
-                className="h-[220px] w-[400px] object-cover"
-               src{''}
-                alt=""
-              />
-            </LazyLoad>
-          </div>
-          <div className="p-4">
-            <div className="flex items-center gap-2 text-sm text-center text-green-500">
-              <p className="w-2 h-2 bg-green-500 rounded-full"></p>
-              <p>Available</p>
+      <h1 className="text-3xl font-medium">Top Fitness Trainers to Book</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Explore our carefully curated list of trusted fitness trainers.
+      </p>
+
+      <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0 ">
+        {trainers.slice(0, 10).map((item, index) => (
+          <div
+            onClick={() => {
+              navigate(`appointment/${item._id}`);
+              scrollTo(0, 0);
+            }}
+            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+            key={index}
+          >
+            <div className="h-[201px] w-full overflow-hidden">
+              <LazyLoad>
+                <img
+                  className="h-[220px] w-[400px] object-cover"
+                  src={item.image}
+                  alt=""
+                />
+              </LazyLoad>
             </div>
-            <p className="text-gray-900 text-lg font-medium">{}</p>
-            <p className="text-gray-600 text-sm">{}</p>
+            <div className="p-4">
+              <div className="flex items-center gap-2 text-sm text-center text-green-500">
+                <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+                <p>Available</p>
+              </div>
+              <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+              <p className="text-gray-600 text-sm">{item.speciality}</p>
+            </div>
           </div>
-        </div>
-      
+        ))}
+      </div>
+      <button
+        onClick={() => {
+          navigate("/trainers");
+          scrollTo(0, 0);
+        }}
+        className="bg-primary text-[#f97000b5] px-12 py-3 rounded-full mt-10"
+      >
+        more
+      </button>
     </div>
-    <button
-     
-      className="bg-primary text-[#f97000b5] px-12 py-3 rounded-full mt-10"
-    >
-      more
-    </button>
-  
-  </>
-
-
-  )
-}
+  );
+};
 
 export default TopTrainers;
