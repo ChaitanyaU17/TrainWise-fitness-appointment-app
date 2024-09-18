2cced66cca760dbf30f9f319f8f7e31b312aaf16
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-18 10:39:19 +0530

    add profile page to app component

--- fitness-appointment/src/App.jsx
+++ fitness-appointment/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
 import MyAppointments from "./pages/MyAppointments";
 import Appointment from "./pages/Appointment";
 import Navbar from "./components/Navbar";
@@ -20,6 +21,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
         <Route path="/my-appointments" element={<MyAppointments />} />
         <Route path="/appointment/:trainerId" element={<Appointment />} />
       </Routes>
