d37581604e6f16590224dca4479ccb0817bc4014
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-18 10:17:14 +0530

    add speciality menu to home page

--- fitness-appointment/src/pages/Home.jsx
+++ fitness-appointment/src/pages/Home.jsx
@@ -2,13 +2,14 @@
 
 
 import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
 
 
 const Home = () => {
   return (
     <div>
       <Header />
-      
+      <SpecialityMenu />
     </div>
   )
 }
