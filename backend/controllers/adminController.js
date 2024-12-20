12d70a8a064351d0d43ced50816d29675d3a82f1
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-12-20 19:15:55 +0530

    validate adminControllers data to add in a database

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,39 +1,39 @@
 import validator from 'validator';
 import bcrypt from 'bcrypt';
 import { v2 as cloudinary } from 'cloudinary';
-import trainerModel from '../models/TrainerModel.js';
+import trainerModel from '../models/trainerModel.js';
 import jwt from 'jsonwebtoken';
 
-//API for adding trainer
-
+// API for adding Trainer
 const addTrainer = async (req, res) => {
+
     try {
-        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
-        const imageFile = req.file;
-        // console.log({ name, email, password, speciality, degree, experience, about, fees, address } ,imageFile);
 
-        //checking for all data to add trainer
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+        const imageFile = req.file
+
+        // checking for all data to add trainer
         if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
-            return res.json({success: false, message: 'Missing Details'})
+            return res.json({ success: false, message: "Missing Details" })
         }
 
-        //validating email format
+        // validating email format
         if (!validator.isEmail(email)) {
-            return res.json({success:false, message: 'Please enter a valid email'})
+            return res.json({ success: false, message: "Please enter a valid email" })
         }
 
-        //validating strong password
+        // validating strong password
         if (password.length < 8) {
-            return res.json({success:false, message: 'Please enter a strong password'})
+            return res.json({ success: false, message: "Please enter a strong password" })
         }
 
-        //hashing trainer password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        // hashing user password
+        const salt = await bcrypt.genSalt(10); // the more no. round the more time it will take
+        const hashedPassword = await bcrypt.hash(password, salt)
 
-        //upload image to cloudinary
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" } );
-        const imageUrl = imageUpload.secure_url;
+        // upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" })
+        const imageUrl = imageUpload.secure_url
 
         const trainerData = {
             name,
@@ -42,39 +42,42 @@ const addTrainer = async (req, res) => {
             password: hashedPassword,
             speciality,
             degree,
-            about,
             experience,
+            about,
             fees,
             address: JSON.parse(address),
             date: Date.now()
         }
 
-        const newTrainer = new trainerModel(trainerData);
-        await newTrainer.save();
-        
-        res.json({success: true, message: 'Trainer Added'});
+        const newTrainer = new trainerModel(trainerData)
+        await newTrainer.save()
+        res.json({ success: true, message: 'Trainer Added' })
 
     } catch (error) {
-        console.log(error);
-        res.json({success: false, message: error.message})
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
 }
 
-// API for admin login 
+
+// API for admin login
 const loginAdmin = async (req, res) => {
     try {
-        const { email, password } = req.body;
-        if( email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD ) {
-            const token = jwt.sign(email+password, process.env.JWT_SECRETE);
-            res.json({ success: true, token});
+
+        const { email, password } = req.body
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET)
+            res.json({ success: true, token })
         } else {
-            res.json({ success: false, message: 'Invalid credentials' });
+            res.json({ success: false, message: "Invalid credentials" })
         }
 
     } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: error.message });
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
+
 }
 
 export { addTrainer, loginAdmin };
