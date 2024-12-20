612e4382f03a88b3ab648c0e0ba8c306c14a5a56
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-12-20 19:18:12 +0530

    in admin authentication middleware if valid token is available it will verify by jwt then handle user authentication by success and error

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -1,24 +1,20 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken"
 
-//admin authentication middleware
-
+// admin authentication middleware
 const authAdmin = async (req, res, next) => {
     try {
-        const { atoken } = req.headers;
+        const { atoken } = req.headers
         if (!atoken) {
-            res.json({success: false, message: 'Not Authorized Login Again'});
+            return res.json({ success: false, message: 'Not Authorized Login Again' })
         }
-        const token_decode = jwt.verify(atoken, process.env.JWT_SECRETE);
-
-        if(token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
-            res.json({success: false, message: 'Not Authorized Login Again'});
+        const token_decode = jwt.verify(atoken, process.env.JWT_SECRET)
+        if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+            return res.json({ success: false, message: 'Not Authorized Login Again' })
         }
-
-        next();
-
+        next()
     } catch (error) {
-        console.log(error);
-        res.json({success: false, message: error.message});
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
 }
 
