e51f7c3083f80701fb60f046e25a69400a8c883d
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-12-29 18:48:35 +0530

    in app context component get trainers data using api and user profile data provide context to the application

--- fitness-appointment/src/context/AppContext.jsx
+++ fitness-appointment/src/context/AppContext.jsx
@@ -1,24 +1,84 @@
-/* eslint-disable react/prop-types */
-import { createContext } from "react";
-import { trainers } from '../assets/assets';
- 
-export const AppContext = createContext();
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import axios from 'axios'
+
+export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const currencySymbol = '$';
+    const currencySymbol = '$'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const [trainers, setTrainers] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false);
+    const [userData, setUserData] = useState(false) 
+
+    // Getting trainers using API
+    const getTrainersData = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/trainer/list');
+             
+            if (data.success) {
+                setTrainers(data.trainers)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
+    }
+
+    // Getting User Profile using API
+    const loadUserProfileData = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+
+            if (data.success) {
+                setUserData(data.userData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
+    }
+
+    useEffect(() => {
+        getTrainersData()
+    }, [])
+
+    useEffect(() => {
+        if (token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false);
+        }
+    }, [token])
 
     const value = {
-        trainers,
-        currencySymbol
+        trainers, getTrainersData,
+        currencySymbol,
+        backendUrl,
+        token, setToken,
+        userData, setUserData, loadUserProfileData
     }
 
     return (
-
         <AppContext.Provider value={value}>
             {props.children}
         </AppContext.Provider>
     )
-};
 
-export default AppContextProvider;
+}
+
+export default AppContextProvider
