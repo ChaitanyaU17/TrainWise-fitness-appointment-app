5311c2abdd3ef240d5e3041f6710d6c5a44938be
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-01-18 09:32:02 +0530

    in appointment model api to get all appointments list

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import { v2 as cloudinary } from 'cloudinary';
 import trainerModel from '../models/trainerModel.js';
 import jwt from 'jsonwebtoken';
+import appointmentModel from '../models/appointmentModel.js';
 
 // API for adding Trainer
 const addTrainer = async (req, res) => {
@@ -92,5 +93,16 @@ const allTrainers = async (req, res) => {
     }
 }
 
-export { addTrainer, loginAdmin, allTrainers };
+//API to get all appointments list
+const appointmentAdmin = async (req, res) => {
+    try {
+        const appointments = await appointmentModel.find({});
+        res.json({success:true, appointments})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message});
+    }
+}
+
+export { addTrainer, loginAdmin, allTrainers, appointmentAdmin };
  
