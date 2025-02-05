47c68a7f729ce67210bcda9b83648de53bae7643
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-02-05 09:43:47 +0530

    trainer route create appointments, complete and cancle appointment route

--- backend/routes/trainerRoute.js
+++ backend/routes/trainerRoute.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { trainerList } from '../controllers/trainerController.js';
+import { loginTrainer, trainerList, appointmentsTrainer, appointmentCancle, appointmentComplete } from '../controllers/trainerController.js';
+import authTrainer from '../middlewares/authTrainer.js';
 
 const trainerRouter = express.Router();
 
-trainerRouter.get('/list', trainerList);
+trainerRouter.post("/login", loginTrainer)
+trainerRouter.get("/list", trainerList)
+trainerRouter.get("/appointments", authTrainer, appointmentsTrainer)
+trainerRouter.post('/complete-appointment', authTrainer, appointmentComplete);
+trainerRouter.post('/cancle-appointment', authTrainer, appointmentCancle);
 
 export default trainerRouter;
