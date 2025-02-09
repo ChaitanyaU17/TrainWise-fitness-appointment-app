8c2b3419e4009f84bf072b67bafcef4e0029d168
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-02-09 12:10:58 +0530

    add navlink to navigate different section

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,36 +1,50 @@
-import { useContext } from "react";
-import { AdminContext } from "../context/AdminContext";
-import { NavLink } from "react-router-dom";
-import { assets } from "../assets/assets";
+import { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+import { TrainerContext } from '../context/TrainerContext'
+import { AdminContext } from '../context/AdminContext'
 
 const Sidebar = () => {
-    const {aToken} = useContext(AdminContext);
+
+  const { dToken } = useContext(TrainerContext)
+  const { aToken } = useContext(AdminContext)
+
   return (
-    <div className="min-h-screen bg-white border-r">
-        { aToken && 
-            <ul className="text-[#515151] mt-5">
-                <NavLink to={'/admin-dashboard'} className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary ' : ''}`}>
-                    <img src={assets.home_icon} alt='' />
-                    <p>Dashboard</p>
-                </NavLink>
+    <div className='min-h-screen bg-white border-r'>
+      {aToken && <ul className='text-[#515151] mt-5'>
 
-                <NavLink to={'/all-appointments'} className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary ' : ''}`}>
-                    <img src={assets.appointment_icon} alt='' />
-                    <p>Appointments</p>
-                </NavLink>
+        <NavLink to={'/admin-dashboard'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+          <img className='min-w-5' src={assets.home_icon} alt='' />
+          <p className='hidden md:block'>Dashboard</p>
+        </NavLink>
+        <NavLink to={'/all-appointments'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+          <img className='min-w-5' src={assets.appointment_icon} alt='' />
+          <p className='hidden md:block'>Appointments</p>
+        </NavLink>
+        <NavLink to={'/add-trainer'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+          <img className='min-w-5' src={assets.add_icon} alt='' />
+          <p className='hidden md:block'>Add Trainer</p>
+        </NavLink>
+        <NavLink to={'/trainer-list'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+          <img className='min-w-5' src={assets.people_icon} alt='' />
+          <p className='hidden md:block'>Trainers List</p>
+        </NavLink>
+      </ul>}
 
-                <NavLink to={'/add-trainer'} className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary ' : ''}`}>
-                    <img src={assets.add_icon} alt='' />
-                    <p>Add Trainer</p>
-                </NavLink>
-
-                <NavLink to={'/trainer-list'} className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary ' : ''}`}>
-                    <img src={assets.people_icon} alt='' />
-                    <p>Trainers List</p>
-                </NavLink>
-
-            </ul>
-        }
+      {dToken && <ul className='text-[#515151] mt-5'>
+        <NavLink to={'/trainer-dashboard'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+          <img className='min-w-5' src={assets.home_icon} alt='' />
+          <p className='hidden md:block'>Dashboard</p>
+        </NavLink>
+        <NavLink to={'/trainer-appointments'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+          <img className='min-w-5' src={assets.appointment_icon} alt='' />
+          <p className='hidden md:block'>Appointments</p>
+        </NavLink>
+        <NavLink to={'/trainer-profile'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
+          <img className='min-w-5' src={assets.people_icon} alt='' />
+          <p className='hidden md:block'>Profile</p>
+        </NavLink>
+      </ul>}
     </div>
   )
 }
