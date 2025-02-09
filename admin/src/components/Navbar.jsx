d19f63d21fa7ecd38df8dfd2eea983584f62b3e2
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-02-09 12:09:49 +0530

    add dToken in navbar section to login trainer

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,25 +1,39 @@
-import { useContext } from "react";
-import { assets } from "../assets/assets";
-import { AdminContext } from "../context/AdminContext";
-import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { TrainerContext } from '../context/TrainerContext'
+import { AdminContext } from '../context/AdminContext'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
-    const {aToken, setAToken} = useContext(AdminContext);
-    const navigate = useNavigate();
 
-    const logout = () => {
-      navigate('/'); 
-      aToken && setAToken('');
-      aToken && localStorage.removeItem('aToken');
-    }
-    
+  const { dToken, setDToken } = useContext(TrainerContext)
+  const { aToken, setAToken } = useContext(AdminContext)
+
+  const navigate = useNavigate()
+
+  const logout = () => {
+    navigate('/')
+    dToken && setDToken('')
+    dToken && localStorage.removeItem('dToken')
+    aToken && setAToken('')
+    aToken && localStorage.removeItem('aToken')
+  }
+
   return (
-    <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
-      <div className="flex items-center gap-2 text-xs">
-        <img className="w-36 sm:w-40 cursor-pointer" src={assets.admin_logo} alt='adminLogo' />
-        <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">{aToken ? 'Admin' : 'Trainer' }</p>
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+      <div className='flex items-center gap-2 text-xs'>
+        <p className="flex items-center cursor-pointer font-black text-xl text-[#f97000]" onClick={() => navigate("/")}>
+                  <img
+                    onClick={() => navigate("/")}
+                    className="w-16 cursor-pointer"
+                    src={assets.logo}
+                    alt=""
+                  />
+                TrainWise
+              </p>
+        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Trainer'}</p>
       </div>
-      <button onClick={logout} className="bg-primary text-white text-sm px-10 py-2 rounded-full ">Logout</button>
+      <button onClick={() => logout()} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
     </div>
   )
 }
