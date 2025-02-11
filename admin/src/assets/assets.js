e1a1ca21bb13f93a4ea87fa5a383ce8c93a7c66c
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-02-11 10:06:13 +0530

    add application assets

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -11,6 +11,9 @@ import tick_icon from './tick_icon.svg'
 import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
+import logo from './logo.png'
+import trainer_logo from './trainer_logo.jpeg'
+import user_logo from './user_logo.jpeg'
 
 export const assets = {
     add_icon,
@@ -25,5 +28,8 @@ export const assets = {
     list_icon,
     tick_icon,
     appointments_icon,
-    earning_icon
+    earning_icon,
+    logo,
+    trainer_logo,
+    user_logo
 }
