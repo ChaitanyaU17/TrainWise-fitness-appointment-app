1ed60c525e3e4243f86a19a49e82007918d94ae0
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-02-11 09:34:28 +0530

    in trainer context component initiate function get trainer  appointments, get trainer profile data, cancle appointment, complete appoitnement

--- admin/src/context/TrainerContext.jsx
+++ admin/src/context/TrainerContext.jsx
@@ -1,19 +1,135 @@
-/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
+import { createContext, useState } from "react";
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
-import { createContext } from "react";
 
-export const TrainerContext = createContext();
+export const TrainerContext = createContext()
 
 const TrainerContextProvider = (props) => {
-    const value = {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const [dToken, setDToken] = useState(localStorage.getItem('dToken') || '');
+    const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
+    const [profileData, setProfileData] = useState(false)
+
+    // Getting Trainer appointment data from Database using API
+    const getAppointments = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/trainer/appointments', {
+                headers: { dToken },
+            });
+            console.log('API Response:', data);
+            if (data.success) {
+                setAppointments(data.appointments);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    };
+
+    // Getting Trainer profile data from Database using API
+    const getProfileData = async () => {
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/trainer/profile', { headers: { dToken } })
+            console.log(data.profileData)
+            setProfileData(data.profileData)
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+    // Function to cancel trainer appointment using API
+    const cancelAppointment = async (appointmentId) => {
+
+        try {
+
+            const { data } = await axios.post(backendUrl + '/api/trainer/cancel-appointment', { appointmentId }, { headers: { dToken } })
+
+            if (data.success) {
+                toast.success(data.message)
+                getAppointments()
+                // after creating dashboard
+                getDashData()
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error)
+        }
 
     }
+
+    // Function to Mark appointment completed using API
+    const completeAppointment = async (appointmentId) => {
+
+        try {
+
+            const { data } = await axios.post(backendUrl + '/api/trainer/complete-appointment', { appointmentId }, { headers: { dToken } })
+
+            if (data.success) {
+                toast.success(data.message)
+                getAppointments()
+                // Later after creating getDashData Function
+                getDashData()
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error)
+        }
+
+    }
+
+    // Getting Trainer dashboard data using API
+    const getDashData = async () => {
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/trainer/dashboard', { headers: { dToken } })
+
+            if (data.success) {
+                setDashData(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
+    }
+
+    const value = {
+        dToken, setDToken, backendUrl,
+        appointments,
+        getAppointments,
+        cancelAppointment,
+        completeAppointment,
+        dashData, getDashData,
+        profileData, setProfileData,
+        getProfileData,
+    }
+
     return (
-        <TrainerContext.Provider value={value} >
+        <TrainerContext.Provider value={value}>
             {props.children}
         </TrainerContext.Provider>
     )
+
+
 }
 
 export default TrainerContextProvider;
