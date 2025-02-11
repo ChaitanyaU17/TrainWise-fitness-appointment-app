b040357037118a57d0393acef2b8ecd722de8520
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-02-11 09:37:31 +0530

    in admin dashboard fix item canclled or not

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -4,9 +4,10 @@ import { assets } from "../../assets/assets";
 import { AppContext } from "../../context/AppContext";
 
 const Dashboard = () => {
-  const { dashData, getDashData, cancleAppointment, aToken } = useContext(AdminContext);
-  
-  const {slotDateFormat} = useContext(AppContext);
+  const { dashData, getDashData, cancleAppointment, aToken } =
+    useContext(AdminContext);
+
+  const { slotDateFormat } = useContext(AppContext);
 
   useEffect(() => {
     if (aToken) {
@@ -18,7 +19,7 @@ const Dashboard = () => {
       <div className="m-5">
         <div className="flex flex-wrap gap-3">
           <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
-            <img className="w-14" src={assets.doctor_icon} alt="" />
+            <img className="w-14" src={assets.trainer_logo} alt="" />
             <div>
               <p className="text-xl font-semibold text-gray-600">
                 {dashData.trainers}
@@ -38,12 +39,12 @@ const Dashboard = () => {
           </div>
 
           <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
-            <img className="w-14" src={assets.patients_icon} alt="" />
+            <img className="w-14" src={assets.user_logo} alt="" />
             <div>
               <p className="text-xl font-semibold text-gray-600">
                 {dashData.patients}
               </p>
-              <p className="text-gray-400">Patient</p>
+              <p className="text-gray-400">User</p>
             </div>
           </div>
         </div>
@@ -56,15 +57,30 @@ const Dashboard = () => {
 
           <div className="pt-4 border border-t-0">
             {dashData.latestAppointments.map((item, index) => (
-              <div className="flex items-center px-6 py-3 gap-3 hover:bg-gray-100" key={index}>
-                <img className="w-10 rounded-full" src={item.trainerData.image} alt="" />
+              <div
+                className="flex items-center px-6 py-3 gap-3 hover:bg-gray-100"
+                key={index}
+              >
+                <img
+                  className="w-10 rounded-full"
+                  src={item.trainerData.image}
+                  alt=""
+                />
                 <div className="flex-1 text-sm">
-                  <p className="text-gray-800 font-medium">{item.trainerData.name}</p>
-                  <p className="text-gray-600">{slotDateFormat(item.slotDate)}</p>
+                  <p className="text-gray-800 font-medium">
+                    {item.trainerData.name}
+                  </p>
+                  <p className="text-gray-600">
+                    {slotDateFormat(item.slotDate)}
+                  </p>
                 </div>
 
                 {item.cancelled ? (
                   <p className="text-red-400 text-xs font-medium">Cancelled</p>
+                ) : item.isCompleted ? (
+                  <p className="text-green-400 text-xs font-medium">
+                    Completed
+                  </p>
                 ) : (
                   <img
                     onClick={() => cancleAppointment(item._id)}
