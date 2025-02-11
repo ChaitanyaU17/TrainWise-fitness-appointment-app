5bb8416a1030c26e1ea47c528a7c1fee709e7bed
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-02-11 09:54:00 +0530

    admin routes for cancle appointment and dashboard

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,15 +1,17 @@
 import express from 'express';
-import { addTrainer, allTrainers, loginAdmin, appointmentAdmin } from '../controllers/adminController.js';
+import { addTrainer, allTrainers, loginAdmin, appointmentAdmin, appointmentCancle, adminDashboard } from '../controllers/adminController.js';
 import upload from '../middlewares/multer.js';
 import authAdmin from '../middlewares/authAdmin.js';
-import { changeAvailability } from '../controllers/trainerController.js';
+import { changeAvailablity } from '../controllers/trainerController.js';
 
 const adminRouter = express.Router();
 
 adminRouter.post('/add-trainer', authAdmin , upload.single('image'), addTrainer);
 adminRouter.post('/login', loginAdmin );
 adminRouter.post('/all-trainers', authAdmin, allTrainers );
-adminRouter.post('/change-availability', authAdmin, changeAvailability );
+adminRouter.post('/change-availability', authAdmin, changeAvailablity );
 adminRouter.get('/appointments', authAdmin, appointmentAdmin);
+adminRouter.post('/cancle-appointment', authAdmin, appointmentCancle);
+adminRouter.get('/dashboard', authAdmin, adminDashboard);
 
 export default adminRouter;
