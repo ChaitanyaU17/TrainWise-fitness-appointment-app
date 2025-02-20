2793f881b77126bcde7213dc947ed934cfe9b0a0
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-02-20 15:17:06 +0530

    fix assets import

--- fitness-appointment/src/assets/assets.js
+++ fitness-appointment/src/assets/assets.js
@@ -1,18 +1,18 @@
-// import Trainer1 from './Trainer1.jpg'
-// import Trainer2 from './Trainer2.jpg'
-// import Trainer3 from './Trainer3.jpg'
-// import Trainer4 from './Trainer4.jpg'
-// import Trainer5 from './Trainer5.jpg'
-// import Trainer6 from './Trainer6.jpg'
-// import Trainer7 from './Trainer7.jpg'
-// import Trainer8 from './Trainer8.jpg'
-// import Trainer9 from './Trainer9.jpg'
-// import Trainer10 from './Trainer10.jpg'
-// import Trainer11 from './Trainer11.jpg'
-// import Trainer12 from './Trainer12.jpg'
-// import Trainer13 from './Trainer13.jpg'
-// import Trainer14 from './Trainer14.jpg'
-// import Trainer15 from './Trainer15.jpg'
+import Trainer1 from './Trainer1.jpg'
+import Trainer2 from './Trainer2.jpg'
+import Trainer3 from './Trainer3.jpg'
+import Trainer4 from './Trainer4.jpg'
+import Trainer5 from './Trainer5.jpg'
+import Trainer6 from './Trainer6.jpg'
+import Trainer7 from './Trainer7.jpg'
+import Trainer8 from './Trainer8.jpg'
+import Trainer9 from './Trainer9.jpg'
+import Trainer10 from './Trainer10.jpg'
+import Trainer11 from './Trainer11.jpg'
+import Trainer12 from './Trainer12.jpg'
+import Trainer13 from './Trainer13.jpg'
+import Trainer14 from './Trainer14.jpg'
+import Trainer15 from './Trainer15.jpg'
 
 import about from './about.jpg';
 import appointment from './appointment.jpg';
@@ -34,7 +34,7 @@ import verified_icon from './verified_icon.svg'
 import arrow_icon from './arrow_icon.svg'
 import info_icon from './info_icon.svg'
 import upload_icon from './upload_icon.png'
-import yoga from './yoga.jpg';
+import yoga from './Yoga.jpg';
 
 
 export const assets = {
